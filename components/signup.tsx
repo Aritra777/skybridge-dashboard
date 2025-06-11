93b76950090f7a2dfb9fe6b796e4d60d3f931c36
Sauradip Ghosh <sauradip96ghosh@gmail.com>
2025-06-11 16:25:13 +0530

    Clerk code shift to signup component

--- components/signup.tsx
+++ components/signup.tsx
@@ -18,6 +18,7 @@ import { Label } from '@/components/ui/label'
 import { Separator } from '@/components/ui/separator'
 import { useState } from 'react'
 import { useSignUp } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation';
 export default function Component() {
   
   // const {isLoaded, signUp, setActive} = useSignUp();
@@ -27,9 +28,60 @@ export default function Component() {
   // const [code,setCode] = useState("");
   // const [error,setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const {isLoaded, signUp, setActive} = useSignUp();
+   const [emailAddress,setEmailAddress] = useState("");
+   const [password,setPassword] = useState("");
+   const [verification,setPendingVerfication] = useState(false);
+   const [code,setCode] = useState("");
+   const [error,setError] = useState("");
 
-  
+   const router = useRouter();
 
+   if(!isLoaded){
+      return null;
+   }
+   async function submit(e: React.FormEvent) {
+      e.preventDefault();
+      if(!isLoaded){
+         return;
+      }
+      try {
+         await signUp.create({
+            emailAddress,
+            password
+         })
+         await signUp.prepareEmailAddressVerification({
+            strategy: "email_code"
+         });
+         setPendingVerfication(true);
+
+      } catch (error: any) {
+         console.log(JSON.stringify(error, null, 2));
+         setError(error.errors[0].message);
+      }
+   }
+
+   async function onPressVerify(e: React.FormEvent) {
+      e.preventDefault();
+      if(!isLoaded){
+         return;
+      }
+      try {
+         const completeSignup = await signUp.attemptEmailAddressVerification({code});
+         if(completeSignup.status !== "complete"){
+            console.log(JSON.stringify(completeSignup,null,2));
+         }
+         if(completeSignup.status === "complete"){
+            //console.log(JSON.stringify(completeSignup));
+            await setActive({session: completeSignup.createdSessionId});
+            router.push("/dashboard");
+         }
+
+      } catch (error: any) {
+         console.log(JSON.stringify(error,null,2));
+         setError(error.errors[0].message);
+      }
+   }
   return (
     <div className="min-h-screen w-full">
       <header className="flex h-16 items-center justify-between px-6 border-b">
